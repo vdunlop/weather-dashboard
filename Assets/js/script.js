d19b4f1f02319ddbc3fc1fd87de8c59a168150ee
vdunlop <148563390+vdunlop@users.noreply.github.com>
2023-12-07 15:32:34 -0600

    Search button working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,20 +1,46 @@
 var openWeatherMapURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 var itemSearch = "";
 
+var searchBtn = document.querySelector("#search");
+
+
 //fetch('https://www.loc.gov/film-and-videos/?q=dog&fo=json&per_page=5', {
 //  fetch(openWeatherMapURL,{
 //})
 //  .then(function (response) {
 //    debugger;
 //    console.log(response);
-    //check for error response
+//check for error response
 
 //    return response.json();
 //  })
 //  .then(function (data) {
-    //data available in here (not out there)
+//data available in here (not out there)
 //    console.log(data);
-    //populateCards();
+//populateCards();
 //  });
 
-console.log("hello world!!!");
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(function () {
+
+
+    function processSearch() {
+        console.log("button clicked");
+
+    }
+
+    // Event listener for the Search button
+    /*searchBtn.addEventListener("click", function (event) {
+        event.stopPropagation();
+        debugger;
+        processSearch();
+    });*/
+    $('.searchBtn').click(function (e) {
+        e.preventDefault();
+ console.log ("in button click");
+ processSearch();
+
+      });
+});
