cf1941f6176bdf5dd2aec66537ad91c9de105a0c
vdunlop <148563390+vdunlop@users.noreply.github.com>
2023-12-09 16:20:54 -0600

    fetch working. current temp, wind, humidity area working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,27 +3,23 @@ const ERROR = -1;
 const NO_ERROR = 0;
 const STATUS_GOOD = 200; // fetch status
 
-// API variables
-var openWeatherMapURL = "api.openweathermap.org/data/2.5/forecast?";
-var latitude = "lat={lat}";
-var longitude = "lon={lon}"
-var appID = "appid={16e7da02b0d8eaa61b87bb1e395ca5cc}";
-var city = "";
-var finalOWMURL = "";
-
 // Set up elements
 var cityTextEl = $("#cityText");
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
     //var searchBtn = document.querySelector("#search");
 
-    function getWeatherDatafromAPI(openWeatherMapURL) {
+    // The API call
+    function processWeatherDatafromAPI(apiURL, cityP) {
         // Fetch the data from the openweathermap api. Input the string that was built that contains
         // the lat and long of the city being searched.
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=16e7da02b0d8eaa61b87bb1e395ca5cc')
-            //  fetch(openWeatherMapURL,{
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.84&lon=93.29&units=imperial&appid=16e7da02b0d8eaa61b87bb1e395ca5cc')
+            //        //fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.84&lon=93.29&appid=9151b72856ea5e674d9bdd5a39dd8f94')
+
+            //  fetch(apiURL,{
             //process response - check for error response
             .then(function (response) {
                 if (response.status !== STATUS_GOOD) {
@@ -31,6 +27,7 @@ $(document).ready(function () {
                     console.log(response);
                     return ERROR;
                 }
+                return response.json();
             })
 
             //process data available from the fetch
@@ -38,6 +35,8 @@ $(document).ready(function () {
                 console.log(data);
                 debugger;
                 //populateCards();
+                formatWeatherBox(cityP, data);
+
             });
     };
 
@@ -46,16 +45,28 @@ $(document).ready(function () {
         return 0;
     }
 
-    function formatWeatherBox(cityP) {
+    function formatWeatherBox(cityP, data) {
 
+        var currentTemp = data.main.temp;
+        var currentHumidity = data.main.humidity;
+        var currentWind = data.wind.speed;
         $("div#cityName").children("p").text(cityP);
-        $("li#temp").text("Temp: " + "temp here");
-        $("li#wind").text("Wind: " + "wind here");
-        $("li#humidity").text("Humidty: " + "humidity here");
+        $("li#temp").text("Temp: " + currentTemp + " F");
+        $("li#wind").text("Wind: " + currentWind + " mph");
+        $("li#humidity").text("Humidity: " + currentHumidity + "%");
         return 0;
     }
 
     function formatFiveDayForecast(cityP) { return 0; }
+
+    function formatWeatherURLCall(newCityP) {
+        // API variables
+        var latitude = "lat=";
+        var longitude = "lon="
+        var newAPIURL = "api.openweathermap.org/data/2.5/forecast?" + "appid=16e7da02b0d8eaa61b87bb1e395ca5cc";
+        return newAPIURL;
+    }
+
     // Process City:
     // Append to list of cities searched for
     // Add to main weather box.
@@ -63,20 +74,27 @@ $(document).ready(function () {
     function processCity(newCityP) {
         var errorRtn = 0;
 
+
         // Append to list of cities searched for
         errorRtn = appendCitytoList(newCityP);
         if (errorRtn == ERROR) {
             return;
         }
 
-        // Add to main weather box.
-        errorRtn = formatWeatherBox(newCityP);
+        var newAPIURL = formatWeatherURLCall(newCityP);
+        errorRtn = processWeatherDatafromAPI(newAPIURL, newCityP);
         if (errorRtn == ERROR) {
             return;
         }
 
+        // Add to main weather box.
+        //errorRtn = formatWeatherBox(newCityP);
+        // if (errorRtn == ERROR) {
+        //     return;
+        // }
+
         // Add 5 day forecast 
-        formatFiveDayForecast(newCityP);
+        // formatFiveDayForecast(newCityP);
     }
 
 
