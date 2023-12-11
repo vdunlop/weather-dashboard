6cb72c90dcc17e2d623f60a04022251bbaf81882
vdunlop <148563390+vdunlop@users.noreply.github.com>
2023-12-10 21:48:20 -0600

    Working copy. Still need to get the lat and long api working, add weather icons and get prev cities to be buttons and local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@
 const ERROR = -1;
 const NO_ERROR = 0;
 const STATUS_GOOD = 200; // fetch status
+const NUM_DAYS = 5; // 5 day forecast
 
 // Set up elements
 var cityTextEl = $("#cityText");
@@ -25,9 +26,6 @@ var cityLS = JSON.parse(localStorage.getItem('cityList'));
 $(document).ready(function () {
 
     // The API calls
-
- 
-
     function processWeatherDatafromAPI(apiURL) {
         // Fetch the data from the openweathermap api. Input the string that was built that contains
         // the lat and long of the city being searched.
@@ -56,11 +54,14 @@ $(document).ready(function () {
 
     // API call to get the latitude and longitude of a city
     // store it in local storage with the city
-        function getLatandLong(apiURL) {
-        /*fetch('https://api.openweathermap.org/geo/1.0/direct?q=Bloomington&limit=1&appid=16e7da02b0d8eaa61b87bb1e395ca5cc')
+    function getLatandLong(apiURL) {
+        /*console.log(apiURL);
+        //fetch('https://api.openweathermap.org/geo/1.0/direct?q=Bloomington&limit=1&appid=16e7da02b0d8eaa61b87bb1e395ca5cc')
+        //fetch('https://geocode.maps.co/search?q=bloomington')
+        fetch(apiURL)
             //process response - check for error response
             .then(function (response) {
-                if (response.status !== 200) {
+                if (response.status !== STATUS_GOOD) {
                     console.log("Fetch error: " + response.status);
                     console.log(response);
                     return response.json();
@@ -70,18 +71,22 @@ $(document).ready(function () {
 
             //process data available from the fetch
             .then(function (data) {
-                console.log(data);
-                console.log(data[0].lat);
-                console.log(data[0].lon);
+                //console.log(data);
+                //console.log(data[0].lat);
+                //console.log(data[0].lon);
+                currentCity.latitude = data[0].lat;
+                currentCity.longitude = data[0].lon;
             });*/
     }
 
     function processFiveDayForecast(apiURL) {
+        console.log(apiURL);
         // Fetch the data from the openweathermap api. Input the string that was built that contains
         // the lat and long of the city being searched.
         //       fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.84&lon=93.29&units=imperial&appid=16e7da02b0d8eaa61b87bb1e395ca5cc')
         //        //fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.84&lon=93.29&appid=9151b72856ea5e674d9bdd5a39dd8f94')
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=44.84&lon=-93.29&units=imperial&appid=16e7da02b0d8eaa61b87bb1e395ca5cc")
+        //fetch("https://api.openweathermap.org/data/2.5/forecast?lat=44.84&lon=-93.29&units=imperial&appid=16e7da02b0d8eaa61b87bb1e395ca5cc")
+        fetch(apiURL)
             //process response - check for error response
             .then(function (response) {
                 if (response.status !== STATUS_GOOD) {
@@ -96,26 +101,78 @@ $(document).ready(function () {
             .then(function (data) {
 
                 //format forecast day boxes
-
+/*
                 console.log(data);
                 console.log(data.list[0].main.temp);
                 console.log(data.list[0].wind.speed);//.wind);
                 console.log(data.list[0].main.humidity);
                 console.log(data.list[0].weather[0].description);//.description);
-                console.log(dayjs(data.list[0].dt_txt).format('M/D/YYYY'));
-$("div#day-one").text(dayjs(data.list[0].dt_txt).format('M/D/YYYY'));                
-$("#day-one").append("<p>Temp: " + data.list[0].main.temp + " F");
-$("#day-one").append("<p>Wind: " + data.list[0].wind.speed + " mph");
-$("#day-one").append("<p>Temp: " + data.list[0].main.humidity + "%");
+                console.log(dayjs(data.list[0].dt_txt).format('M/D/YYYY'));*/
+                var currentDate = dayjs(data.list[0].dt_txt).format('M/D/YYYY');
+                var dataListIndex = 0; // points to the data that came back from the API call
+                var daysIndex = 0;  // points to the 5 day forecast day (1 through 5)
 
+                // set day one of the 5 day forecast
+                $("div#day-one").text(dayjs(data.list[daysIndex].dt_txt).format('M/D/YYYY'));
+                $("#day-one").append("<p>Temp: " + data.list[daysIndex].main.temp + " F");
+                $("#day-one").append("<p>Wind: " + data.list[daysIndex].wind.speed + " mph");
+                $("#day-one").append("<p>Temp: " + data.list[daysIndex].main.humidity + "%");
 
+                // look for the next date in the API call data and store it in the 5 day forecast
+                var loopNotEnd = true;
+                while (loopNotEnd) {
+                    dataListIndex++;  // next API return
+                    nextDate = dayjs(data.list[dataListIndex].dt_txt).format('M/D/YYYY');  // next date in the API return
+                    //console.log(nextDate);
+                    //console.log(currentDate);
+                    //console.log(dataListIndex);
+                    //console.log(loopNotEnd);
+                    //console.log(daysIndex);
+
+                    // new date, then put the data in there
+                    if (currentDate < nextDate) {
+                        currentDate = nextDate;
+                        daysIndex++;
+                        switch (daysIndex) {
+                            case 2: $("div#day-two").text(dayjs(data.list[daysIndex].dt_txt).format('M/D/YYYY'));
+                                $("#day-two").append("<p>Temp: " + data.list[daysIndex].main.temp + " F");
+                                $("#day-two").append("<p>Wind: " + data.list[daysIndex].wind.speed + " mph");
+                                $("#day-two").append("<p>Temp: " + data.list[daysIndex].main.humidity + "%");
+                                break;
+                            case 3: $("div#day-three").text(dayjs(data.list[daysIndex].dt_txt).format('M/D/YYYY'));
+                                $("#day-three").append("<p>Temp: " + data.list[daysIndex].main.temp + " F");
+                                $("#day-three").append("<p>Wind: " + data.list[daysIndex].wind.speed + " mph");
+                                $("#day-three").append("<p>Temp: " + data.list[daysIndex].main.humidity + "%");
+                                break;
+                            case 4: $("div#day-four").text(dayjs(data.list[daysIndex].dt_txt).format('M/D/YYYY'));
+                                $("#day-four").append("<p>Temp: " + data.list[daysIndex].main.temp + " F");
+                                $("#day-four").append("<p>Wind: " + data.list[daysIndex].wind.speed + " mph");
+                                $("#day-four").append("<p>Temp: " + data.list[daysIndex].main.humidity + "%");
+                                break;
+                            case 5: $("div#day-five").text(dayjs(data.list[daysIndex].dt_txt).format('M/D/YYYY'));
+                                $("#day-five").append("<p>Temp: " + data.list[daysIndex].main.temp + " F");
+                                $("#day-five").append("<p>Wind: " + data.list[daysIndex].wind.speed + " mph");
+                                $("#day-five").append("<p>Temp: " + data.list[daysIndex].main.humidity + "%");
+                                break;
+                            default:
+                                console.log("Error in ProcessFiveDayForecast switch");
+                        }
+                    }
+                    dataListIndex++;
+
+                    // filled up all 5 forecast days, then we're done
+                    if (daysIndex === NUM_DAYS) {
+                        loopNotEnd = false;
+                    }
+                }
+                return 0;
             });
     }
 
 
-
+    // Format the current weather box
     function formatWeatherBox() {
-        console.log("in format weather box");
+        //console.log("in format weather box");
 
         var today = dayjs();
         $("div#cityName").children("p").text(currentCity.name + " " + today.format('M/D/YYYY'));
@@ -123,8 +180,8 @@ $("#day-one").append("<p>Temp: " + data.list[0].main.humidity + "%");
         $("li#wind").text("Wind: " + currentCity.wind + " mph");
         $("li#humidity").text("Humidity: " + currentCity.humidity + "%");
 
-        console.log("currentcity");
-        console.log(currentCity);
+        //console.log("currentcity");
+        //console.log(currentCity);
         return 0;
     }
 
@@ -132,73 +189,79 @@ $("#day-one").append("<p>Temp: " + data.list[0].main.humidity + "%");
     // get lat and long
     // fetch current weather for lat and long (city)
     // format current weather on screen
-    function processCurrentWeather() {
+    function processWeather() {
         console.log("in process current weather");
         // Get latitude and longitude for our API call for weather
-        var latLongAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=Bloomington&limit=1&appid=16e7da02b0d8eaa61b87bb1e395ca5cc';
-        var errorRtn = getLatandLong(latLongAPI);
+        debugger;
+        var latLongAPI = 'http://geocode.maps.co/search?q=' + currentCity.name;
+        getLatandLong(latLongAPI);
         console.log("currentcity");
         console.log(currentCity);
         debugger;
 
         // format the URL to get the weather
-        var latitude = "lat=" + currentCity.latitude;
-        var longitude = "lat=" + currentCity.longitude;
+        //var latitude = "lat=" + currentCity.latitude;
+        //var longitude = "lat=" + currentCity.longitude;
         var longitude = "lon=" + "-93.29";
         var latitude = "lat=" + "44.84";
         var newAPIURL = "https://api.openweathermap.org/data/2.5/weather?" + latitude + "&" + longitude + "&units=imperial&" + "appid=16e7da02b0d8eaa61b87bb1e395ca5cc";
 
         console.log("in process current weather");
         console.log(newAPIURL);
+        debugger;
         // fetch current weather for lat and long (city) and format on screen
         errorRtn = processWeatherDatafromAPI(newAPIURL);
         if (errorRtn == ERROR) {
-            console.log("Error return in processCurrentWeather");
+            console.log("Error return in processWeather");
             return;
         }
+
+            // Process the 5 day forecast
+            $("#fiveDayForecast").css("display", "flex");
+
+            debugger;
+        // format the URL to get the 5 day forecast
+        //var latitude = "lat=" + currentCity.latitude;
+        //var longitude = "lat=" + currentCity.longitude;
+        var longitude = "lon=" + "-93.29";
+        var latitude = "lat=" + "44.84";
+        var newAPIURL = "https://api.openweathermap.org/data/2.5/forecast?" + latitude + "&" + longitude + "&units=imperial&" + "appid=16e7da02b0d8eaa61b87bb1e395ca5cc";
+
+        console.log(newAPIURL);
+        errorRtn = processFiveDayForecast(newAPIURL);
+    
     }
 
     // Append to list of cities searched for
     function appendCitytoList() {
-        console.log("in append");
+        //console.log("in append");
         $("#cityList").append('<button class="listCityBtn">' + currentCity.name + '</button>');
         return 0;
     }
 
-    // Process 5 day Forecast:
-    function formatFiveDayForecast() {
-        return 0;
-    }
-
-
-    // Process City:
-    // Append to list of cities searched for
+     // Process City:
+    // Append city to list of cities searched for
     // store in local storage
     // process the weather 
+    // process the 5 day forecast
     function processCity() {
-        console.log("in process city");
+        //console.log("in process city");
         var errorRtn = 0;
 
         errorRtn = appendCitytoList();
         if (errorRtn == ERROR) {
             return;
         }
-        console.log(currentCity)
+        //console.log(currentCity)
         // Add city to local storage
 
         //Process the current weather
-        errorRtn = processCurrentWeather();
+        errorRtn = processWeather();
 
-        // Process the 5 day forecast
-        $("#fiveDayForecast").css("display","flex");
-
-        debugger;
-        errorRtn = processFiveDayForecast();
-
+    
     }
 
     // Process Search clicked
-    // Search button was clicked. 
     // Get input city and start the whole process
     function processSearch() {
         console.log("button clicked");
@@ -221,7 +284,7 @@ $("#day-one").append("<p>Temp: " + data.list[0].main.humidity + "%");
             humidity: ""
         };
         // Process the city that was entered.
-        console.log(currentCity);
+        //console.log(currentCity);
         processCity();
     }
 
